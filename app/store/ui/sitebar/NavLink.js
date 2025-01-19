71d9afa84b41ad3397e4a6d64841bd923330bd00
RifatBinBaccu9 <rifatbinbaccu@gmail.com>
2025-01-19 20:18:02 +0600

    sdfa

--- app/store/ui/sitebar/NavLink.js
+++ app/store/ui/sitebar/NavLink.js
@@ -27,7 +27,6 @@ const NavLink = () => {
             <Link href={link.href} key={idx} className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-purple-600 md:flex-none md:justify-start md:p-2 md:px-3">
                <NavIcon />
                 <h1>{link.name}</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque aliquid magni quam, nesciunt odit enim unde ratione, soluta nam asperiores similique numquam sed tempora quod nihil praesentium voluptatibus! Voluptatum, enim?</p>
             </Link>
           )
         })
